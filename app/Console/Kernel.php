ec6e8986518c11c9ac62c9e3d3bde2e89c697611
halimatussadia <halima.sadia245@gmail.com>
2022-02-13 13:26:54 +0600

    scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('weekly:mail_report')
+            ->weekly()
+            ->appendOutputTo('schedule.log');
     }
 
     /**
